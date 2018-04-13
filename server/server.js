cd03eef1e66924f3cd511dcd4bf93d59f87ddd27
unknown <admin@VLAN-25-58-306.rd.mirea.ru>
2018-04-13 20:46:08 +0300

    filevfile

---
+++ server/server.js
@@ -0,0 +1,8 @@
+﻿var http = require("http");
+var fs = require("fs");
+var body = fs.readFileSync("../pr1.html","utf-8"); 
+http.createServer(function(request, response){
+    response.setHeader("Content-Type","text/html"); 
+	response.write(body);
+	response.end();
+}).listen(80);
